6e0fc2a820e91ffb2c44bb58bd0fa2d8275a55ac
Ben Kreis <benjaminkreis@gmail.com>
2018-07-17 11:16:38 -0500

    for conv2d

--- convert_data_to_txt.py
+++ convert_data_to_txt.py
@@ -44,37 +44,91 @@ if __name__ == "__main__":
     # To use one data file:
     h5File = h5py.File(options.inputFile)
     treeArray = h5File[options.tree][()]
-    
+
     # List of features to use
     # List of features to use
-    features = yamlConfig['Inputs']
+    #features = yamlConfig['Inputs']
     
     # List of labels to use
-    labels = yamlConfig['Labels']
+    #labels = yamlConfig['Labels']
 
     # Convert to dataframe
-    features_df = pd.DataFrame(treeArray,columns=features)
-    labels_df = pd.DataFrame(treeArray,columns=labels)
-    
-    # Convert to numpy array with correct shape
-    features_val = features_df.values
-    labels_val = labels_df.values
+    #features_df = pd.DataFrame(treeArray,columns=features)
+    #labels_df = pd.DataFrame(treeArray,columns=labels)
 
-    X_train_val, X_test, y_train_val, y_test = train_test_split(features_val, labels_val, test_size=0.2, random_state=42)
-    print X_train_val.shape
-    print y_train_val.shape
-    print X_test.shape
-    print y_test.shape
+    #Copy processing from Maurizio    
+    jets = h5File.get('jets')
+    jetImagePt = np.array(h5File.get('jetImagePt'))
+    print(jets.shape, jetImagePt.shape)
+
+    # prepare target
+    target = jets[:,-6:-1]
+    print(target.shape)
+    
+    # prepare expert-feature array
+    expFeature = jets[:,1:4]
+    print(expFeature.shape)
+
+    def shuffle(a, b, c):
+        iX = a.shape[1]
+        iY = a.shape[2]
+        b_shape = b.shape[1]
+        a = a.reshape(a.shape[0], iX*iY)
+        total = np.column_stack((a,b))
+        total = np.column_stack((total,c))
+        np.random.shuffle(total)
+        a = total[:,:iX*iY]
+        b = total[:,iX*iY:iX*iY+b_shape]
+        c = total[:,iX*iY+b_shape:]
+        a = a.reshape(a.shape[0], iX, iY, 1)
+        return a,b,c
+
+    # shuffle datasets
+    jetImagePt, expFeature, target = shuffle(jetImagePt, expFeature, target)
+
+    from sklearn.preprocessing import MinMaxScaler
+    ## normalize expert features inputs and outputs between 0 and 1
+    scaler = MinMaxScaler()
+    print(scaler.fit(expFeature))
+    maxVal = scaler.data_max_
+    minVal = scaler.data_min_
+    expFeature = (expFeature-minVal)/(maxVal-minVal)
+    # normalize the images dividing by the maximum pT
+    jetImagePt = jetImagePt/maxVal[0]
+
+    iSplit = int(0.7*target.shape[0])
+    x_image_train = jetImagePt[:iSplit, :, :]
+    x_image_test = jetImagePt[iSplit:, :, :]
+    x_train = expFeature[:iSplit, :]
+    x_test = expFeature[iSplit:, :]
+    y_train = target[:iSplit, :]
+    y_test = target[iSplit:, :]
+    
+    print(x_image_train.shape)
+
+    print 'END PROCESSING'
+
+    # Convert to numpy array with correct shape
+    #features_val = features_df.values
+    #labels_val = labels_df.values
+
+    #X_train_val, X_test, y_train_val, y_test = train_test_split(features_val, labels_val, test_size=0.2, random_state=42)
+    #print X_train_val.shape
+    #print y_train_val.shape
+    #print X_test.shape
+    #print y_test.shape
 
     #Normalize
-    if yamlConfig['NormalizeInputs']:
-     scaler = preprocessing.StandardScaler().fit(X_train_val)
-     X_test = scaler.transform(X_test) 
+    #if yamlConfig['NormalizeInputs']:
+    # scaler = preprocessing.StandardScaler().fit(X_train_val)
+    # X_test = scaler.transform(X_test) 
              
     modelName = options.inputModel.split('/')[-1].replace('.h5','')
     
+    print options.inputModel
     model = load_model(options.inputModel)
-    predict_test = model.predict(X_test)
+    print 'LOADED MODEL***********************************'
+    predict_test = model.predict(x_image_test)
 
     print "Writing",y_test.shape[1],"predicted labels for",y_test.shape[0],"events in outfile",(options.outputDir+'/'+modelName+'_truth_labels.dat')  
     outf_labels = open(options.outputDir+'/'+modelName+'_truth_labels.dat','w')
